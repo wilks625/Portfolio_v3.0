a07b65b28f03d48242d4050022da7a90ee96830f
Stephen Yacas <syacas123@gmail.com>
2021-07-18 22:07:47 -0400

    updated footer and navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import { MDBIcon, MDBContainer } from 'mdbreact';
+import React from "react";
+import { MDBIcon, MDBContainer } from "mdbreact";
 
-
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col } from "react-bootstrap";
 // import 'bootstrap/dist/css/bootstrap.min.css'
-import '../App.css'
-import { SocialIcon } from 'react-social-icons';
-
-
+import "../App.css";
+import { SocialIcon } from "react-social-icons";
 
 const SocialButtonsPage = () => {
   return (
-      <footer id="footer">
-<SocialIcon id="socialIcon" url="https://www.facebook.com/stephen.yacas" />
-<SocialIcon id="socialIcon" url="https://www.linkedin.com/in/stephen-yacas-bb5557113/" />
-<SocialIcon id="socialIcon" url="https://github.com/wilks625" />
-
-
+    <footer id="footer">
+      <SocialIcon
+        id="socialIcon"
+        url="https://www.facebook.com/stephen.yacas"
+      />
+      <SocialIcon
+        id="socialIcon"
+        url="https://www.linkedin.com/in/stephen-yacas-bb5557113/"
+      />
+      <SocialIcon id="socialIcon" url="https://github.com/wilks625" />
     </footer>
   );
-}
+};
 
 export default SocialButtonsPage;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,30 @@
 import React from "react";
-import '../App.css'
+import "../App.css";
 
 // import logo from "../logo2.png"
-import{ Nav } from 'react-bootstrap'
-import { Link } from 'react-router-dom';
+import { Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-
 const Navbar = () => {
-    return (
-        <Nav className="navbar navbar-expand-lg navbar-light bg-dark">
-<div className="container">
-
-
+  return (
+    <Nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className="container">
         {/* <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..." /></a> */}
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }}/>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
         </button>
-      
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             {/* <Nav.Item className="nav-item active ">
@@ -39,7 +44,9 @@ const Navbar = () => {
             </Nav.Item> */}
 
             <li className="nav-item">
-              <a className="nav-link" href="/">Home</a>
+              <a className="nav-link" href="/">
+                Home
+              </a>
             </li>
 
             {/* <li className="nav-item">
@@ -47,36 +54,41 @@ const Navbar = () => {
             </li> */}
 
             <li className="nav-item">
-              <a className="nav-link" href="/projects">Projects</a>
+              <a className="nav-link" href="/projects">
+                Projects
+              </a>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" href="/contact">Contact Info</a>
+              <a className="nav-link" href="/contact">
+                Contact Info
+              </a>
             </li>
 
             <li className="nav-item">
-            <a className="nav-link" style={{display: "table-cell"}} href="https://docs.google.com/document/d/1KMeS7QJI3yYgY8yl-7ELAQMNj5XT5EaXHCyboXHDCEE/edit?usp=sharing" target="_blank">Resume</a>
+              <a
+                className="nav-link"
+                style={{ display: "table-cell" }}
+                href="https://docs.google.com/document/d/1KMeS7QJI3yYgY8yl-7ELAQMNj5XT5EaXHCyboXHDCEE/edit?usp=sharing"
+                target="_blank"
+              >
+                Resume
+              </a>
             </li>
-            
-
-
 
             {/* <Nav.Item className="nav-item "> */}
-           
-              {/* <Link>
+
+            {/* <Link>
             <a id="navButton" value="Open Window"
             onclick="window.open('https://docs.google.com/document/d/1KMeS7QJI3yYgY8yl-7ELAQMNj5XT5EaXHCyboXHDCEE/edit?usp=sharing')">Resume</a>
             </Link> */}
-           
+
             {/* </Nav.Item> */}
-
           </ul>
-         
         </div>
-    </div>
-      </Nav>
-      
-    )
-}
+      </div>
+    </Nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
